ce66609e6899653216b105137273e1ade8c3d7da
rcarrz04 <ruben04@stanford.edu>
2025-04-25 02:48:52 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/ee-portfolio">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
