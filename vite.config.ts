7dfd4952e968ddae79ab6dca89303f1780ea0f81
rcarrz04 <ruben04@stanford.edu>
2025-05-03 11:49:24 -0700

    Restore working Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,29 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/ee-portfolio/",
+
+  server: {
+    port: 8080,
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+      output: {
+        manualChunks: undefined,
+        assetFileNames: "assets/[name].[ext]",
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+      },
     },
   },
 });
