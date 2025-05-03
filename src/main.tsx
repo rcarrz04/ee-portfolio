1e78f05c47b3713e26b0de5030cf16ed8fd6faa8
rcarrz04 <ruben04@stanford.edu>
2025-05-03 11:48:32 -0700

    Remove test component and simplify main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// Add a test component to verify base URL
-const TestComponent = () => {
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>Test Component</h1>
-      <p>If you can see this, the base URL is working correctly!</p>
-    </div>
-  )
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TestComponent />
     <App />
   </React.StrictMode>,
 )
