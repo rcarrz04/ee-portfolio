17ebf7102654066b2ce1e36d651ec79faee9a42a
rcarrz04 <ruben04@stanford.edu>
2025-10-25 01:07:46 -0700

    Fix cursor to appear only on top line while typing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -55,7 +55,7 @@ const Home = () => {
                 speed={50}
                 className="text-4xl font-bold text-white font-sfpro"
                 repeat={0}
-                cursor={false}
+                cursor={true}
               />
               <TypeAnimation
                 sequence={[
@@ -67,7 +67,7 @@ const Home = () => {
                 speed={50}
                 className="text-2xl font-bold text-white font-sfpro"
                 repeat={0}
-                cursor={true}
+                cursor={false}
               />
             </div>
           </div>
